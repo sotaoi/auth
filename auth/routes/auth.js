e1d3d010c052aa5c008a7e9a8213797c186b1b9f
qwertypnk <qwertypnk@protonmail.com>
2021-10-04 10:51:00 +0300

    refactor: update

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const DebugControl = require("../utilities/debug.js");
+const express = require('express');
+const DebugControl = require('../utilities/debug.js');
 
 let router;
 
 const authRoutes = (oauthServer, authorize) => {
-  if (typeof router !== "undefined") {
+  if (typeof router !== 'undefined') {
     return router;
   }
 
   router = express.Router(); // Instantiate a new router
 
-  router.get("/", (req, res) => {
+  router.get('/', (req, res) => {
     // send back a simple form for the oauth
     res.send({ ok: true });
   });
 
   router.post(
-    "/authorize",
+    '/authorize/:repository',
     async (req, res, next) => {
-      DebugControl.log.flow("Initial User Authentication");
+      DebugControl.log.flow('Initial User Authentication');
       try {
-        const user = await authorize(req.body);
+        const user = await authorize(req.params.repository, req.body);
         if (user) {
           req.body.user = user;
           return next();
@@ -33,36 +33,34 @@ const authRoutes = (oauthServer, authorize) => {
       };
       [
         // Send params back down
-        "client_id",
-        "redirect_uri",
-        "response_type",
-        "grant_type",
-        "state",
+        'client_id',
+        'redirect_uri',
+        'response_type',
+        'grant_type',
+        'state',
       ].map((a) => (result[a] = req.body[a]));
       return res.send({ ...result });
     },
     (req, res, next) => {
       // sends us to our redirect with an authorization code in our url
-      DebugControl.log.flow("Authorization");
+      DebugControl.log.flow('Authorization');
       return next();
     },
     oauthServer.authorize({
       authenticateHandler: {
         handle: (req) => {
-          DebugControl.log.functionName("Authenticate Handler");
-          DebugControl.log.parameters(
-            Object.keys(req.body).map((k) => ({ name: k, value: req.body[k] }))
-          );
+          DebugControl.log.functionName('Authenticate Handler');
+          DebugControl.log.parameters(Object.keys(req.body).map((k) => ({ name: k, value: req.body[k] })));
           return req.body.user;
         },
       },
-    })
+    }),
   );
 
   router.post(
-    "/token",
+    '/token',
     (req, res, next) => {
-      DebugControl.log.flow("Token");
+      DebugControl.log.flow('Token');
       next();
     },
     oauthServer.token({
@@ -70,7 +68,7 @@ const authRoutes = (oauthServer, authorize) => {
         // whether client needs to provide client_secret
         // 'authorization_code': false,
       },
-    })
+    }),
   ); // Sends back token
 
   return router;

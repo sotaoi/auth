0a442be2daddedabeece60eba4bee82606d6b6f3
qwertypnk <qwertypnk@protonmail.com>
2021-10-17 09:25:03 +0300

    fix: smallfix

--- auth/app.js
+++ auth/app.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const oauthServerInit = require("./oauth/server.js");
-const { setPort } = require("./port");
-const DebugControl = require("./utilities/debug.js");
+const express = require('express');
+const oauthServerInit = require('./oauth/server.js');
+const { setPort } = require('./port');
+const DebugControl = require('./utilities/debug.js');
 
 const startAuthServer = (port, provider, authorize) => {
   setPort(port);
@@ -10,26 +9,25 @@ const startAuthServer = (port, provider, authorize) => {
   const app = express();
   const oauthServer = oauthServerInit(provider);
 
-  //Here we are configuring express to use body-parser as middle-ware.
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+  app.use(app.urlencoded({ extended: false }));
+  app.use(app.json());
   app.use(DebugControl.log.request());
 
-  app.use("/client", require("./routes/client.js")); // Client routes
-  app.use("/oauth", require("./routes/auth.js")(oauthServer, authorize)); // routes to access the auth stuff
+  app.use('/client', require('./routes/client.js')); // Client routes
+  app.use('/oauth', require('./routes/auth.js')(oauthServer, authorize)); // routes to access the auth stuff
   // Note that the next router uses middleware. That protects all routes within this middleware
   app.use(
-    "/oauth/verify",
+    '/oauth/verify',
     (req, res, next) => {
-      DebugControl.log.flow("Authentication");
+      DebugControl.log.flow('Authentication');
       return next();
     },
     oauthServer.authenticate(),
     (req, res) => {
       res.send({ success: true });
-    }
+    },
   ); // routes to access the protected stuff
-  app.use("/", (req, res) => res.redirect("/client"));
+  app.use('/', (req, res) => res.redirect('/client'));
 
   app.listen(port);
   console.log(`Oauth Server listening on port ${port}`);
